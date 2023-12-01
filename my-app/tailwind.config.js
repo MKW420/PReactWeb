025a22c97677facb393922d1cd4cf3b77ac30b96
MKW420 <keitumetseshongwe@gmail.com>
2023-12-01 13:38:53 +0200

    -Project setup

---
+++ my-app/tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+//all paths to template files to be added here
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
